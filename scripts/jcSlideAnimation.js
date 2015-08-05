bcaa54235290494c1633b6bbb26c01c5654ddb27
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-08-04 21:24:33 -0700

    Adding the slide animation code.

---
+++ scripts/jcSlideAnimation.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    angular.module('app').animation('.jc-slide', [function() {
+      return {
+        // make note that other events (like addClass/removeClass)
+        // have different function input parameters
+        add: function(element, doneFn) {
+          jQuery(element).slideDown(1000, doneFn);
+        },
+        remove: function(element, doneFn) {
+          jQuery(element).slideUp(1000, doneFn);
+        }
+      };
+    }]);
+
+})();
