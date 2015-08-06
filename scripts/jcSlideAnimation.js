5cc940f37c2ab0f1567a084154afcf7c0bca1e56
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-08-05 21:06:31 -0700

    Slide up is working correctly.

--- scripts/jcSlideAnimation.js
+++ scripts/jcSlideAnimation.js
@@ -3,11 +3,17 @@
       return {
         // make note that other events (like addClass/removeClass)
         // have different function input parameters
-        add: function(element, doneFn) {
+        enter: function(element, doneFn) {
           $(element).slideDown(1000, doneFn);
         },
-        remove: function(element, doneFn) {
-          $(element).slideUp(1000, doneFn);
+        addClass : function(element, className, done) {
+            if(className == 'ng-hide') {
+                $(element).slideUp(1000, done);
+            }
+            else {
+                $(element).slideDown(1000, done);
+            }
+            return function onEnd(element, done) { };
         }
       };
     }]);
