a98f277648bca0e3da8f36cc6bbbad499b5d07f1
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-08-05 20:35:20 -0700

    Fixed an issue with bower installing an older version of angular animate.

--- scripts/jcSlideAnimation.js
+++ scripts/jcSlideAnimation.js
@@ -1,17 +1,13 @@
-(function () {
-    'use strict';
 
     angular.module('app').animation('.slide', [function() {
       return {
         // make note that other events (like addClass/removeClass)
         // have different function input parameters
         add: function(element, doneFn) {
-          jQuery(element).slideDown(1000, doneFn);
+          $(element).slideDown(1000, doneFn);
         },
         remove: function(element, doneFn) {
-          jQuery(element).slideUp(1000, doneFn);
+          $(element).slideUp(1000, doneFn);
         }
       };
     }]);
-
-})();
