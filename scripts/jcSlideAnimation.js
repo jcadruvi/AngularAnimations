5f27ce0c0144e2004849c634923ac14106d147f0
Josh Cadruvi <jcadruvi78@yahoo.com>
2015-08-09 17:57:13 -0700

    slide animation is working except for first time.

--- scripts/jcSlideAnimation.js
+++ scripts/jcSlideAnimation.js
@@ -1,19 +1,21 @@
 
     angular.module('app').animation('.slide', [function() {
       return {
-        // make note that other events (like addClass/removeClass)
-        // have different function input parameters
-        enter: function(element, doneFn) {
-          $(element).slideDown(1000, doneFn);
-        },
-        addClass : function(element, className, done) {
-            if(className == 'ng-hide') {
-                $(element).slideUp(1000, done);
+            beforeAddClass : function(element, className, done) {
+                if(className == 'ng-hide') {
+                    jQuery(element).slideUp(1000, done);
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass : function(element, className, done) {
+                if(className == 'ng-hide') {
+                    jQuery(element).slideDown(1000, done);
+                }
+                else {
+                    done();
+                }
             }
-            else {
-                $(element).slideDown(1000, done);
-            }
-            return function onEnd(element, done) { };
-        }
       };
     }]);
